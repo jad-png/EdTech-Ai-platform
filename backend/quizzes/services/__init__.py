de0c4a73150cd06687518e02868877aa4dc60cd9
jad-png <j.belassiria3993@uca.ac.ma>
2026-08-08 01:08:47 +0100

    refactor: remove unused generate_content import from quiz services

--- backend/quizzes/services/__init__.py
+++ backend/quizzes/services/__init__.py
@@ -1,10 +1,8 @@
-from .llm import generate_content
 from .generator import generate_quiz_for_document
 from .grading import start_quiz_attempt, submit_and_grade_attempt
 from .analytics import get_user_learning_analytics
 
 __all__ = [
-    "generate_content",
     "generate_quiz_for_document",
     "start_quiz_attempt",
     "submit_and_grade_attempt",
